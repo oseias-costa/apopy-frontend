348f6a4f46d414c1c9774aa6561a3e48210ca2fe
Oseias Costa <oseiascosta@Mac-Pro-de-Oseias.local>
2023-08-03 08:34:11 -0300

    feat(hooks): create hooks for useAppSelector

--- src/presentation/pages/ProductsPage/components/CreateProduct.tsx
+++ src/presentation/pages/ProductsPage/components/CreateProduct.tsx
@@ -1,19 +1,27 @@
-import { useState, useEffect } from "react"
+import { createSelector } from "@reduxjs/toolkit"
+import { useCallback, useEffect, useRef, useState } from "react"
 import { useSelector } from "react-redux"
 import { Modal } from "../../../components/global/Modal"
+import { useAppSelector } from "../../../hooks/hooks"
 import { ProductCreate } from "../../../types/pages/products.types"
 
 export const CreateProduct = ({state, setState}) => {
     const [ product, setProduct ] = useState<ProductCreate>({
         name: '', category: '', subcategory: '', suplier: '', _id: '' })
+    const [ stateProduct, setStateProduct ] = useState(false)
+        const useaa = useRef(stateProduct)
 
-    let teste     
-        // useEffect(() => {
-        //     const stateProduct = useSelector(state =>  state)
-        //     teste = stateProduct
-        // }, [])
+        const { products } = useAppSelector((state) => {
+            return state.product
+        })
 
-        console.log(teste)
+        const produtsTwo = (state) => state.product.products
+
+        const selectArr = createSelector([produtsTwo], (producttwo) => {
+            console.log(producttwo)
+            return producttwo.product.products
+        })
+    
 
     return(
         <>
