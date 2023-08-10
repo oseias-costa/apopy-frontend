3665cb81793704952c88c6e31f06335917de0a0b
Oseias Costa <oseiascosta@Mac-Pro-de-Oseias.local>
2023-08-10 13:23:07 -0300

    feat(TransferToSale): create component

---
+++ src/presentation/pages/SalesPage/components/TransferToSale.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react"
+import { transferSaleUseCase } from "../../../../application/sale.usecase";
+import { StockStateProps } from "../../../types/pages/stock.types"
+
+interface NewSale {
+    quantity: number;
+    price: number;
+    profit: number;
+    percentage: number;
+    createAt: string
+}
+
+const initialStateNewSale: NewSale = {
+    quantity: 0,
+    price: 0,
+    profit: 0,
+    percentage: 0,
+    createAt: ''
+}
+
+export const TransferToSale: React.FC<StockStateProps> = ({ stockState, setStockState }) => {
+    const [ newSale, setNewSale ] = useState<NewSale>(initialStateNewSale)
+    
+    async function handleTransferSale(){
+        const data = await transferSaleUseCase(stockState)
+
+        if(data.status === 200){
+            console.log(data)
+        }
+    }
+
+    return (
+            <div>Transfer Modal
+                <input type='number' value={newSale.quantity} onChange={(e) => setNewSale({ ...newSale, quantity: Number(e.target.value)})} />
+                <input type='number' value={newSale.price} onChange={(e) => setNewSale({ ...newSale, price: Number(e.target.value)})} />
+                <input type='number' value={newSale.price} onChange={(e) => setNewSale({ ...newSale, price: Number(e.target.value)})} />
+                <input type='number' value={newSale.profit} onChange={(e) => setNewSale({ ...newSale, profit: Number(e.target.value)})} />
+                <input type='text' value={newSale.createAt} onChange={(e) => setNewSale({ ...newSale, createAt: e.target.value})} />
+                <button>Nova Venda</button>
+            </div>
+    )
+}
--- src/presentation/pages/StockPage/components/MovimentStockItem.tsx
+++ src/presentation/pages/StockPage/components/MovimentStockItem.tsx
@@ -1,10 +1,19 @@
 import { Modal } from "../../../components/global/Modal"
 import { StockStateProps } from "../../../types/pages/stock.types"
+import { TransferToSale } from "../../SalesPage/components/TransferToSale"
 
 export const MovimentStockItem: React.FC<StockStateProps> = ({stockState, setStockState}) => {
     return(
         <Modal state={stockState} setState={setStockState}>
-            <div>MovimentItem</div>
+            <input disabled={true} value={stockState.product} />
+            <input disabled={true} value={stockState.category} />
+            <input disabled={true} value={stockState.subcategory} />
+            <input disabled={true} value={stockState.suplier} />
+            <input disabled={true} value={stockState.quantity} />
+            <input disabled={true} value={stockState.total} />
+            <input disabled={true} value={stockState.costPrice} />
+
+            <TransferToSale stockState={stockState} setStockState={setStockState} />
         </Modal>
     )
 }
