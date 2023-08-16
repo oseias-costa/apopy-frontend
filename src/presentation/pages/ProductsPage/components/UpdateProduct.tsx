11c06cc8be37570672ef24e7edcb8ee6a97fd30c
Oséias Costa <oseiasc2@gmail.com>
2023-08-16 22:15:38 +0000

    fix(UpdateProduct): update component with style

--- src/presentation/pages/ProductsPage/components/UpdateProduct.tsx
+++ src/presentation/pages/ProductsPage/components/UpdateProduct.tsx
@@ -1,27 +1,61 @@
 import { Modal } from "../../../components/global/Modal";
-import { initialStateProducts, ProductStateProps } from "../../../types/pages/products.types";
+import {
+  initialStateProducts,
+  ProductStateProps,
+} from "../../../types/pages/products.types";
 import { useGetCategories } from "../../../hooks/useGetCategories";
 import { useGetSuplier } from "../../../hooks/useGetSupliers";
 import { SuplierSelect } from "./SuplierSelect";
 import { CategorySelect } from "./CategorySelect";
 import { SubcategorySelect } from "./SubcategorySelect";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { updateProduct } from "../../../redux/slice/productSlice";
 import { updateProductUseCase } from "../../../../application/product.usecase";
+import * as S from "../../../styles/GlobalStyles/modal.style";
+import { SpinnerIcon } from "../../../assets/icons/SpinnerIcon";
+import { useEffect, useState } from "react";
+import { ProductComponentState } from "../../../types/pages/stock.types";
+import {
+  verifyIfHaveEmptyFields,
+  verifyProductEqualNewProduct,
+} from "./utils-product";
+import { RootState } from "../../../redux/store";
 
-export const UpdateProduct: React.FC<ProductStateProps> = ({ state, setState }) => {
+export const UpdateProduct: React.FC<ProductStateProps> = ({
+  state,
+  setState,
+}) => {
   const dispatch = useDispatch();
   const { supliers } = useGetSuplier();
   const { categories } = useGetCategories();
+  const productItemUpdate = useSelector((stateProduct: RootState) =>
+    stateProduct.product.products.filter((item) => item._id === state._id)
+  )[0];
+  const [productComponentState, setProductComponentState] =
+    useState<ProductComponentState>({
+      isEmpty: true,
+      loading: false,
+    });
   const verify = supliers.length === 0 && categories.length === 0;
 
-  console.log('ver id', state)
+  console.log("productItem", productItemUpdate);
+  console.log("state", state);
+
+  useEffect(() => {
+    if (
+      verifyIfHaveEmptyFields(state) &&
+      !verifyProductEqualNewProduct(state, productItemUpdate)
+    ) {
+      setProductComponentState({ isEmpty: false, loading: false });
+    } else {
+      setProductComponentState({ isEmpty: true, loading: false });
+    }
+  }, [state]);
 
   async function handleCreateProduct() {
-    const update = await updateProductUseCase({...state});
+    const update = await updateProductUseCase({ ...state });
 
     if (update.status === 200) {
-      
       dispatch(updateProduct(update.data?.data.updateProduct));
       setState(initialStateProducts);
     }
@@ -30,21 +64,37 @@ export const UpdateProduct: React.FC<ProductStateProps> = ({ state, setState }) 
   return (
     <>
       <Modal state={state} setState={setState}>
-        {verify ? (
-          <div>Você precisa criar categorias e adicionar supliers</div>
-        ) : (
-          <div>
-            <h1>Editar Produto</h1>
-            <input
-              value={state?.name}
-              onChange={(e) => setState({ ...state, name: e.target.value })}
-            />
-            <CategorySelect product={state} setProduct={setState} />
-            <SubcategorySelect product={state} setProduct={setState} />
-            <SuplierSelect product={state} setProduct={setState} />
-            <button onClick={() => handleCreateProduct()}>Editar Produto</button>
-          </div>
-        )}
+        <S.ModalContent>
+          <S.TitleModal>
+            <S.TitleModalH2>Editar Produto</S.TitleModalH2>
+          </S.TitleModal>
+          {verify ? (
+            <div>Você precisa criar categorias e adicionar supliers</div>
+          ) : (
+            <>
+              <S.ModalContentText>
+                Para Editar o produto, faça as alterações e clique em editar.
+              </S.ModalContentText>
+              <CategorySelect product={state} setProduct={setState} />
+              <SubcategorySelect product={state} setProduct={setState} />
+              <SuplierSelect product={state} setProduct={setState} />
+              <S.InputModal
+                value={state?.name}
+                onChange={(e) => setState({ ...state, name: e.target.value })}
+              />
+              <S.ButtonModal
+                disabled={productComponentState.isEmpty}
+                onClick={() => handleCreateProduct()}
+              >
+                {productComponentState.loading ? (
+                  <SpinnerIcon />
+                ) : (
+                  "Editar Produto"
+                )}
+              </S.ButtonModal>
+            </>
+          )}
+        </S.ModalContent>
       </Modal>
     </>
   );
--- src/presentation/pages/ProductsPage/components/utils-product.ts
+++ src/presentation/pages/ProductsPage/components/utils-product.ts
@@ -1,10 +1,25 @@
-import { ProductCreateState } from "../../../types/pages/stock.types"
+import { Product } from "../../../../domain/entities/product";
+import { ProductCreateState } from "../../../types/pages/stock.types";
 
-export const verifyIfHaveEmptyFields = (product: ProductCreateState): boolean => {
-    const category = product.category.length >= 3
-    const subcategory = product.subcategory.length >= 3
-    const suplier = product.suplier.length >= 3
-    const productName = product.name.length >= 3
+export const verifyIfHaveEmptyFields = (
+  product: ProductCreateState
+): boolean => {
+  const category = product.category.length >= 3;
+  const subcategory = product.subcategory.length >= 3;
+  const suplier = product.suplier.length >= 3;
+  const productName = product.name.length >= 3;
 
-    return Boolean(category && subcategory && suplier && productName)
-} 
+  return Boolean(category && subcategory && suplier && productName);
+};
+
+export const verifyProductEqualNewProduct = (
+  state: ProductCreateState,
+  product: Product
+) => {
+  const category = product.category === state.category;
+  const subcategory = product.subcategory === state.subcategory;
+  const suplier = product.suplier === state.suplier;
+  const productName = product.name === state.name;
+
+  return Boolean(category && subcategory && suplier && productName);
+};
