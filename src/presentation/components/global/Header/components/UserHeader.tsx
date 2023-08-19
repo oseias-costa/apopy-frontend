f62a0e6ac1212b7c441a11dc082a13f004f80aed
Oséias Costa <oseiasc2@gmail.com>
2023-08-19 02:37:07 +0000

    fix(user-header.style): rename style file

--- src/presentation/components/global/Header/components/UserHeader.tsx
+++ src/presentation/components/global/Header/components/UserHeader.tsx
@@ -1,31 +1,32 @@
-import { useDispatch, useSelector } from "react-redux"
-import { fetchUser, initialUserState } from "../../../../redux/slice/userSlice"
-import { RootState } from "../../../../redux/store"
-import * as S from "../../../../styles/ComponentsStyles/userHeader"
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUser, initialUserState } from "../../../../redux/slice/userSlice";
+import { RootState } from "../../../../redux/store";
+import * as S from "../../../../styles/ComponentsStyles/user-header.style";
+import { ButtonModal } from "../../../../styles/GlobalStyles/modal.style";
 
 export const UserHeader = () => {
-    const dispatch = useDispatch()
-    const user = useSelector((state: RootState)=> state.user.user)
-    const sizeName = user.name?.split(" ").length - 1;
+  const dispatch = useDispatch();
+  const user = useSelector((state: RootState) => state.user.user);
+  const sizeName = user.name?.split(" ").length - 1;
 
-    function handleLogout() {
-        localStorage.removeItem("apopyToken");
-        dispatch(fetchUser(initialUserState.user));
-      }
+  function handleLogout() {
+    localStorage.removeItem("apopyToken");
+    dispatch(fetchUser(initialUserState.user));
+  }
 
-
-    return(
-        <S.UserHeaderContainer>
-            <S.UserAbreviation>
-                <S.UserIconCircle>
-                    {user.name?.split(" ")[0][0]}
-                    {user.name?.split(" ")[sizeName][0]}
-                </S.UserIconCircle>
-            </S.UserAbreviation>
-            <S.UserNameText>{user.name}</S.UserNameText>
-            <S.UserEmailText>{user.email}</S.UserEmailText>
-            <button onClick={() => handleLogout()}>Logout</button>
-        </S.UserHeaderContainer>
-    )
-}
-
+  return (
+    <S.UserHeaderContainer>
+      <S.UserAbreviation>
+        <S.UserIconCircle>
+          {user.name?.split(" ")[0][0]}
+          {user.name?.split(" ")[sizeName][0]}
+        </S.UserIconCircle>
+      </S.UserAbreviation>
+      <S.UserNameText>{user.name}</S.UserNameText>
+      <S.UserEmailText>{user.email}</S.UserEmailText>
+      <ButtonModal disabled={false} onClick={() => handleLogout()}>
+        Logout
+      </ButtonModal>
+    </S.UserHeaderContainer>
+  );
+};
