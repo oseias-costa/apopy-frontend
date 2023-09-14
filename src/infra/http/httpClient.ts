74b3073291bb6db7582b9c91f1427e21eebdb6c5
Oséias Costa <oseiasc2@gmail.com>
2023-09-14 22:22:34 +0000

    fix(httpClient): fixed adress api

--- src/infra/http/httpClient.ts
+++ src/infra/http/httpClient.ts
@@ -4,7 +4,8 @@ export async function httpClient(query: string, variables: any) {
   const token: string | null = localStorage.getItem("apopyToken");
 
   const data = axios({
-    url: "https://apopy-api.onrender.com/graphql",
+    // url: "https://apopy-api.onrender.com/graphql",
+    url: "https://vast-pear-shrimp-gear.cyclic.app/graphql",
     method: "post",
     data: {
       query: `${query}`,
